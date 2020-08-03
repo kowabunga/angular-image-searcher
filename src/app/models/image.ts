f803fd3bc8073cb769f8d5233a5a3a09e96b2b3e
Anthony Siletti <anthony.siletti@gmail.com>
2020-08-02 20:01:35 -0400

    link fixes

--- src/app/models/image.ts
+++ src/app/models/image.ts
@@ -32,27 +32,22 @@ export class Image {
         this.imageLarge = image.largeImageURL;
         this.pageUrl = image.pageURL;
         this.photographer = image.user;
-        this.photographerUrl = `https://pixabay.com/users/${image.user}-${image.id}`;
+        this.photographerUrl = `https://pixabay.com/users/${image.user}-${image.user_id}/`;
         this.downloads = image.downloads;
         this.favorites = image.favorites;
         this.likes = image.likes;
         this.tagStr = image.tags;
         break;
       case 'unsplash':
-        console.log(image);
         //unsplash has
         // likes, tags,
         this.id = image.id.toString();
         this.type = type;
         this.imageSmall = image.urls.small;
         this.imageLarge = image.urls.raw;
-        this.pageUrl = image.links.html.concat(
-          '/?utm_source=AngularImageSearcher&utm_medium=referral'
-        );
+        this.pageUrl = `${image.links.html}/?utm_source=AngularImageSearcher&utm_medium=referral`;
         this.photographer = image.user.name;
-        this.photographerUrl = image.user.links.html.concat(
-          '/?utm_source=AngularImageSearcher&utm_medium=referral'
-        );
+        this.photographerUrl = `${image.user.links.html}/?utm_source=AngularImageSearcher&utm_medium=referral`;
         this.likes = image.likes;
         this.tagArr = image.tags;
         break;
